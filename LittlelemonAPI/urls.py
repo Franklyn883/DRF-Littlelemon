333ae2009d0015e9e8ddb8691032fca9fd374051
Frank Alimimian <alimimianfrank@gmail.com>
2023-06-09 10:38:18 +0100

    Add ordering, sorting, and searching features to class-based view in Django Rest Framework

--- LittlelemonAPI/urls.py
+++ LittlelemonAPI/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('menu-items', views.MenuItemsView.as_view()),
-    # path('menu-items/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('menu-items', views.menu_items),
-    # path('menu-items/<int:pk>', views.menu_item),
-    # path('category-detail/<int:pk>', views.category_detail, name='category-detail'),
-    ##path('menu', views.menu, name="menu")
+path('menu-items', views.MenuItemsViewSet.as_view({'get':'list'}), name="menu-items"),
+path('menu-items/<int:pk>',views.MenuItemsViewSet.as_view({'get':'retrieve'})),
 ]
